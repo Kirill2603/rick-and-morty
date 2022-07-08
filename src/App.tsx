b5c0603ad28bf4085beb1ebbf85ae159f272b3de
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-07-08 11:08:38 +0300

    add getAllPersons and mini pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { api, getPersonsResponse } from './api/rickAndMortyApi'
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <Persons />
+    </QueryClientProvider>
+  )
+}
+
+function Persons() {
+
+  const [page, setPage] = useState<number>(1)
+
+  const { isLoading, isError, data, error, } = useQuery<getPersonsResponse, Error>(
+    ['persons', page],
+    () => api.getPersons(page),
+    { keepPreviousData : true })
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error?.message}</span>
+  }
+
+  return (
+    <>
+      <div>Persons :</div>
+      <div>Find {data?.info.count} persons</div>
+      <div>{data?.info.pages} pages</div>
+
+      <input type='number' value={page} onChange={(event) => {setPage(Number(event.target.value))}}/> <button>go to</button>
+
+      {data?.results.map(({ id, name, image, status }) =>
+        <div key={id}>
+          <img src={image} alt='name' />
+          <p>{name}</p>
+          <p>status : {status}</p>
+        </div>)}
+    </>
+
+
+  )
 }
---
+++ src/api/rickAndMortyApi.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+export type getPersonsResponse = {
+  info: {
+    count: number,
+    pages: number,
+    next: string,
+    prev: string | null,
+  },
+  results: PersonType[]
+}
+
+export type PersonType = {
+  id: number,
+  name: string,
+  status: string,
+  species: string,
+  type: string,
+  gender: string,
+  origin: {
+    name: string,
+    url: string,
+  },
+  location: {
+    name: string,
+    url: string,
+  },
+  image: string,
+  episode: string[],
+  url: string,
+  created: string
+}
+
+const apiInstance = axios.create({
+  baseURL: 'https://rickandmortyapi.com/api/'
+})
+
+export const api = {
+ async getPersons(page: number, ) {
+    const {data} = await apiInstance.get<getPersonsResponse>('character/', {
+      params: {page}
+    })
+    return data
+  }
+}
